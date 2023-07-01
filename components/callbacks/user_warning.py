09a5916ec7223f96909043b04ec6990b05f4b3fb
Victoria Hartwick <victoria.hartwick@gmail.com>
2023-07-01 12:56:48 -0700

    added user_warning.py, a security testing callback to perform whitelist input validation for lat, lon, ls, and tod user inputs

--- components/callbacks/user_warning.py
+++ components/callbacks/user_warning.py
@@ -10,75 +10,108 @@ import re
 
 # Load Database
 dv = df.var_data()
+dp = df.plotting_options()
+
 
 # Option to Link to Submit Button and Check all 
 @app.callback(
     [Output('lat-alert', 'children'),
-    Output('lat-alert', 'is_open')],
-    Input("lat-input", "value"),
+    Output('lat-alert', 'is_open'),
+    Output('lon-alert', 'children'),
+    Output('lon-alert', 'is_open'),
+    Output('ls-alert', 'children'),
+    Output('ls-alert', 'is_open'),
+    Output('tod-alert', 'children'),
+    Output('tod-alert', 'is_open'),  
+    Output('clev-alert', 'children'),
+    Output('clev-alert', 'is_open'),
+    Output('btn-doit-txt', 'disabled')],
+    [Input("lat-input", "value"),
+    Input("lon-input", "value"),
+    Input("solar-longitude-input", "value"),
+    Input("tod-input", "value"),
+    Input("clev-input", "value")],
     State("plot-type-dropdown", "value"),
 )
-def user_warning(lat_input, plot_input):
+
+def user_warning(lat_input,lon_input,ls_input, tod_input,clev_input,plot_input):
+
+   alert_message = ["None","None","None","None","None"]
+   alert_is_open = [False, False, False, False, False]
+   var_list = ['lat','lon','time','time_of_day']
+   input_list = [lat_input, lon_input, ls_input, tod_input]
+   button_status = False   # the plot button is enabled by default
+
+   for i in range(len(var_list)):
+      
+      # First check formattting
+      alert_message[i],alert_is_open[i], button_status = format_check(plot_input,var_list[i],input_list[i])
+  
+   if any(alert_is_open): # checks if any formatting alerts are triggered
+      return alert_message[0], alert_is_open[0], alert_message[1], alert_is_open[1], alert_message[2], alert_is_open[2], alert_message[3], alert_is_open[3], alert_message[4], alert_is_open[4], button_status
+
+   for i in range(len(var_list)):
+      # Next Check Values
+      alert_message[i], alert_is_open[i], button_status = range_check(i,var_list[i],input_list[i])
+      
+   return alert_message[0], alert_is_open[0], alert_message[1], alert_is_open[1], alert_message[2], alert_is_open[2], alert_message[3], alert_is_open[3], alert_message[4], alert_is_open[4], button_status
+
+def format_check(plot_input,var_name,var_input):
+   aio, am, btns = False, "", True     
 
    # FIND DIMENSION NAME
    dimx = dv.loc[(dv['plot-type']==plot_input),'dimx'].values[0]
    dimy = dv.loc[(dv['plot-type']==plot_input),'dimy'].values[0]
 
-   # check if latittude is a dimension
-   if 'lat' in dimx or 'lat' in dimy:
-
-
-      # define he regular expression to match the user input
-      input_pattern = r'^(\d+,\d+|ALL)$'
-                    
-      # check format
-      if re.match(input_pattern, lat_input):
-         alert_message = ""
-         alert_is_open = False
-      else:
-         alert_message = "User input is not in the expected format. Separate values for the latitude range by a single comma with no spaces or, to include the entire latitude range, type ALL"
-         alert_is_open = True
-
+   # Define the regular expression to match the user input
+   if var_name in dimx or var_name in dimy:   # if user variable is one of the plot dimensions
+      input_pattern = r'^(-?\d+,-?\d+|ALL)$'
+      format_error_message = "User Input is not in the expected format. Separate values for the latitude range by a single comma with no spaces or, to include the entire latitude range, type ALL"
    else:
-      # define the regular expression to match the user input
-      input_pattern = r'^(\d+,\d+|\d+|ALL)$'
+      input_pattern = r'^(-?\d+,-?\d+|-?\d+|ALL)$'
+      format_error_message = "User Input is not in the expected format. You can specify a single numeric value (#), a range of values separated by a single comma with no spaces, or to include the entire latitude range, type ALL"
+    
+   # check format & return if not matching expected format
+   if not re.match(input_pattern, var_input):
+      aio = True
+      am = format_error_message
+      btns = aio
+   return am, aio, btns
+   
+def range_check(i, var_name, var_input):
 
-      # check format
-      if re.match(input_pattern, lat_input):
-         alert_message = ""
-         alert_is_open = False       
-      else:
-         alert_message = "User Input is not in the Expected Format. You can specify a single value (#), a range of values separated by a comma, or to include the entire range, type ALL"
-
-
-   # Check if value is within expected range or in the correct format (-90,90)
-   if "," and "ALL" not in lat_input:  # single value selected
+   ### BIG WARNING CHANGE RANGE MAX FOR TOD BASED ON SIZE OF DIURN FILE! ###
+   range_max = [90,360,360,24]
+   range_min = [-90,0,0,0]
+   rem_list = ["Latitude should be between -90 and 90", "Longitude should be between 0 and 360",
+              "Aerocentric longitude (Ls) should be between 0 and 360","Local Times should be between 0 and 24"]
+   aio, am = False, ""    
+   
+   # Check that values are within expected range
+   range_error_message = rem_list[i]          
+         
+   if "," not in var_input and "ALL" not in var_input:  # single value selected
       try:  
-         if int(lat_input) < -90 or int(lat_input) > 90:
-            raise ValueError("Latitude should be between -90 and 90")
-         alert2_message = ""
-         alert2_is_open = False
+         if int(var_input) < range_min[i] or int(var_input) > range_max[i]:
+            raise ValueError(range_error_message)
       except ValueError as e:
-         alert2_message = str(e)
-         alert2_is_open = True
+         am = str(e)
+         aio = True
 
-   elif lat_input == 'ALL':   # average over ALL
-      alert2_message = ""
-      alert2_is_open = False
+   elif var_input == 'ALL':   # average over ALL
+      aio = False
 
    else: # range
-      dim_split = str(lat_input).split(",")
+      dim_split = str(var_input).split(",")
       try:
-         if dim_split[0] < -90 or dim_split[0] > 90 or dim_split[1] < -90 or dim_split[1] > 90:
-            raise ValueError("Latitude should be between -90 and 90")
-         alert2_message = ""
-         alert2_is_open = False
+         if int(dim_split[0]) < range_min[i] or int(dim_split[0]) > range_max[i] or int(dim_split[1]) < range_min[i] or int(dim_split[1]) > range_max[i]:
+            raise ValueError(range_error_message)
       except ValueError as e:
-         alert2_message = str(e)
-         alert2_is_open = True
+         am = str(e)
+         aio = True
 
-   # Combine alerts & return any that are open
-   alert_message = alert_message+alert2_message
-   alert_is_open = alert_is_open or alert2_is_open
-   return alert_message, alert_is_open
+   btns= aio
+
+   # return any that are open 
+   return am, aio, btns
 
